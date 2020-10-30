2271adab1542e22f79c2b4024c508ebdf13e9511
Ruth Poliakon <r.poliakon@gmail.com>
2020-10-29 18:09:40 -0600

    base error handling added

--- index.js
+++ index.js
@@ -20,6 +20,19 @@ server.get("/", (req, res, next) => {
   });
 });
 
+server.use((req, res) => {
+  res.status(404).json({
+    message: "Route was not found",
+  });
+});
+
+server.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).json({
+    message: "An internal error occurred, please try again later",
+  });
+});
+
 server.listen(port, host, () => {
   console.log(`\n*** Server Running on http://${host}:${port} ***\n`);
 });
